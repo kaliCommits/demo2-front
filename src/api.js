2c41ccdd12ffbf413352060f42a3408b54188384
kaliCommits <kalilinux.darkweb@gmail.com>
2023-05-08 10:47:27 +0530

    netli

--- src/api.js
+++ src/api.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 
+let url = "";
+if(process.env.NODE_ENV === 'production'){
+  console.log("prod");
+  url = process.env.REACT_APP_PRODUCTION_BACK_URL;
+}else{
+  console.log("dev");
+  url = process.env.REACT_APP_DEV_BACK_URL;
+}
+
 export const apiReq = axios.create({
   // baseURL
   // baseURL:"https://demo1-backend.onrender.com",
   // baseURL:"http://localhost:4000/api/v1",
-  baseURL: "demo2-production.up.railway.app/api/v1",
+  baseURL: url,
   withCredentials: true,
-  // headers: {
-  //   "Access-Control-Allow-Origin": "*",
-  //   "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-  // },
 });
